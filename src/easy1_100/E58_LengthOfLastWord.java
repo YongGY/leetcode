2cd2ed811e220cff7de73a8d8748082cb701b0d7
William <715guoyong@gmail.com>
2017-10-23 18:37:16 -0700

    .

--- src/easy1_100/E58_LengthOfLastWord.java
+++ src/easy1_100/E58_LengthOfLastWord.java
@@ -17,13 +17,16 @@ package easy1_100;
 public class E58_LengthOfLastWord {
 
     public static int lengthOfLastWord(String s) {
-		return 0;
-        
+    	String[] a = s.split(" ");
+    	if(a == null || a.length == 0) {
+        	return 0;
+    	}
+    	return a[a.length-1].length();
     }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(lengthOfLastWord("Hello World"));
 	}
 
 }
---
+++ src/easy1_100/E66_PlusOne.java
@@ -0,0 +1,41 @@
+package easy1_100;
+
+import java.util.Arrays;
+
+/**
+ * Given a non-negative integer represented as a non-empty array of digits, plus
+ * one to the integer.
+ * 
+ * You may assume the integer do not contain any leading zero, except the number
+ * 0 itself.
+ * 
+ * The digits are stored such that the most significant digit is at the head of
+ * the list.
+ * 
+ * @author William
+ *
+ */
+public class E66_PlusOne {
+
+	public static int[] plusOne(int[] digits) {
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] != 9) {
+				digits[i]++;
+				return digits;
+			} else {
+				digits[i] = 0;
+			}
+		}
+
+		int[] re = new int[digits.length + 1];
+		re[0] = 1;
+
+		return re;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Arrays.toString(plusOne(new int[] {3,2,9})));
+	}
+
+}
