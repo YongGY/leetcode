5d58775c9fd500ee633ee7b2e207a63deac115c8
William <715guoyong@gmail.com>
2018-07-08 14:53:35 -0700

    .

---
+++ src/array/sort/A05_280WiggleSort.java
@@ -0,0 +1,21 @@
+package array.sort;
+
+import java.util.List;
+
+import common.Interval;
+
+/**
+Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+ * 
+ * @author William
+ *
+ */
+public class A05_280WiggleSort {
+
+	public void wiggleSort(int[] nums) {
+		
+	}
+
+}
