b45a9940d7830791a8a3cb18b886238ed2572bde
William <715guoyong@gmail.com>
2018-07-11 08:32:16 -0700

    ...

--- src/string/basic/A23_316RemoveDuplicateLetters.java
+++ src/string/basic/A23_316RemoveDuplicateLetters.java
@@ -1,15 +1,25 @@
 package string.basic;
 
 /**
+ * Given a string which contains only lowercase letters, remove duplicate letters so that every letter appear once and only once. 
+ * You must make sure your result is the smallest in lexicographical order among all possible results.
  * 
+ * Example 1:
+ * Input: "bcabc"
+ * Output: "abc"
  * 
+ * Example 2:
+ * Input: "cbacdcbc"
+ * Output: "acdb"
  * 
  * @author William
  *
  */
-public class A01_028ImplementStrStr {
+public class A23_316RemoveDuplicateLetters {
 	
-	
+    public String removeDuplicateLetters(String s) {
+        
+    }
 	
 	
 	
