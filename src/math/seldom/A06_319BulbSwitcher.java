e38d32a3eced5164f29b92061d40f9cd74579442
William <715guoyong@gmail.com>
2018-08-07 13:55:14 -0700

    .

--- src/math/seldom/A06_319BulbSwitcher.java
+++ src/math/seldom/A06_319BulbSwitcher.java
@@ -6,10 +6,10 @@ package common;
  * @author William
  *
  */
-public class ClassExample {
+public class A06_319BulbSwitcher {
 	
     
  	public static void main(String[] args) {
- 		
+ 		System.out.println(12313132);
 	}
 }
